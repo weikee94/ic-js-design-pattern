43cba6fcedbf257c4f459cb7402960d18dd5ffdf
weikee94 <weikee070194@gmail.com>
2020-07-17 16:56:48 +0800

    js polymorphism

--- src/index.js
+++ src/index.js
@@ -1,1 +1,65 @@
-console.log("index");
+class People {
+  constructor(name, age) {
+    this.name = name;
+    this.age = age;
+  }
+  getName = () => {
+    return this.name;
+  };
+
+  getAge = () => {
+    return this.age;
+  };
+}
+
+let p = new People("haha", 12);
+console.log(p.getName());
+console.log(p.getAge());
+
+class Student extends People {
+  constructor(name, age, number) {
+    super(name, age);
+    this.number = number;
+  }
+  study = () => {
+    console.log(`${this.name} with id ${this.number} study `);
+  };
+}
+
+let ali = new Student("ali", 22, "110000");
+console.log(ali.study());
+console.log(ali.getAge());
+
+class Shape {
+  constructor(name) {
+    this.name = name;
+  }
+
+  area = () => {};
+}
+
+class Triangle extends Shape {
+  constructor(name) {
+    super(name);
+  }
+
+  area = (b, h) => {
+    return (b * h) / 2;
+  };
+}
+
+let t1 = new Triangle("t1");
+console.log(`${t1.name} area is: ${t1.area(5, 10)}`);
+
+class Square extends Shape {
+  constructor(name) {
+    super(name);
+  }
+
+  area = (l) => {
+    return l * l;
+  };
+}
+
+let s1 = new Square("s1");
+console.log(`${s1.name} area is ${s1.area(10)}`);
--- webpack.dev.config.js
+++ webpack.dev.config.js
@@ -1,7 +1,31 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
 module.exports = {
   entry: "./src/index.js",
   output: {
     path: __dirname,
     filename: "./release/bundle.js",
+  },
+  module: {
+    rules: [
+      // 用babel transform es6
+      {
+        test: /\.js?$/,
+        exclude: /(node_modules)/,
+        loader: "babel-loader",
+      },
+    ],
+  },
+  plugins: [
+    // 生成模版
+    new HtmlWebpackPlugin({
+      template: "./index.html",
+    }),
+  ],
+  devServer: {
+    contentBase: path.join(__dirname, "./release"), // 根目录
+    open: true,
+    port: 9000,
   },
 };
